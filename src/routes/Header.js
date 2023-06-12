10b3ae7dba46663b2b3eca6547c9a2f43e02611b
Jakub <zkuba94@gmail.com>
2023-06-12 19:48:46 +0200

    created main structure of a project

---
+++ src/routes/Header.js
@@ -0,0 +1,10 @@
+import '../styles/Header.css';
+function Header() {
+    return (
+        <>
+            <h1>Real Pizza Price</h1>
+            <p>Compare real price and size of pizzas, by filling these fields with data.</p>
+        </>
+    );
+}
+export default Header;
